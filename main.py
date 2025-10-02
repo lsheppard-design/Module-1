7719b403c7bfbef387fa8ef24369a045eb4bbba8
Lili <lsheppard@eugenia.com>
2025-10-02 13:12:20 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+age = 34
+height = 1.69
+name = "lili"
+is_student = True
+print(f"Age: {age}, Type: {type(age)}")
+print(f"height: {height}, Type: {type(height)}")
+print(f"name: {name}, Type: {type(name)}")
+print(f"is_student: {is_student}, Type: {type(is_student)}")
+
+a = 10
+b = 30
+print("addition:", a + b)
+print("substraction:", a-b)
+print("multiplication:", a*b)
+print("division:", a/b)
+print(14 % 5)
+print(8 ** 3)
+
+first_name = "lili"
+last_name = "sheppard"
+identite = first_name + " " + last_name
+print(identite)
+full_name = first_name + " " + last_name
+print(full_name.upper())
+full_name = "Lili Sheppard"
+print(len(full_name))
+phrase = "je m appelle lili"
+mot = phrase.split()
+print(mot)
+
+age_str = "24"
+age_int = int(age_str)
+print(age_str)
+print(f"Age_int: {age_int}, Type: {type(age_int)}")
+number_float = 1.69
+number_int = 2
+print(number_float)
+print(f"number_int: {number_int}, Type: {type(number_int)}")
+number_int = 2
+number_float = float(number_int)
+print(number_float)
+age = 24
+age_str = str(age)
+print(f"age_str: {age_str}, Type: {type(age_str)}")
+
+lili = True
+lillie = False
+print("lili and lillie =", lili and lillie)
+print("lili or lillie =", lili or lillie)
+print("not lillie =", not lillie)
+a = 25
+b = 70
+
+
+
